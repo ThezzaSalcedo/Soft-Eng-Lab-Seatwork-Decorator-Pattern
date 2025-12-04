70e3b2627084bb24490576188defa08d84e6d741
Thezzalia Salcedo <salcedothezza@gmail.com>
2025-12-05 00:26:51 +0800

    Update CoffeeDecorator.java

--- CoffeeDecorator.java
+++ CoffeeDecorator.java
@@ -1,17 +1,3 @@
-abstract class CoffeeDecorator implements Coffee {
-private Coffee coffee;
-
-    public CoffeeDecorator(Coffee coffee) {
-        this.coffee = coffee;
-    }
-
-    @Override
-    public String getDescription() {
-        return coffee.getDescription();
-    }
-
-    @Override
-    public double getCost() {
-        return coffee.getCost();
-    }
+public interface CoffeeDecorator extends Coffee {
+    void setCoffee(Coffee coffee);
 }
